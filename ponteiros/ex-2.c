d302e8584017395f26f9ce8bc9a7d9af84caca0e
Vitor da Penha <103641736+d9light@users.noreply.github.com>
2025-05-15 09:48:03 -0300

    Correção ex-2.c

--- ponteiros/ex-2.c
+++ ponteiros/ex-2.c
@@ -1,3 +1,4 @@
+
 /******************************************************************************
 
 Faça uma função que receba um número N como parâmetro por valor e 2 inteiros p1
@@ -6,48 +7,69 @@ N e em p2 o menor número primo maior que N.
 
 *******************************************************************************/
 #include <stdio.h>
+#include<stdbool.h>
 
-void maiorPrimoEmenorPrimo(int valor, int *p1, int *p2){
-    int i;
-    int cont;
-    
-    for(i = 1; i <= valor; i++){
-       
-        if(valor % i == 0){
-            
+
+bool ehPrimo(int contador){
+    int cont = 0;
+
+    for(int i = 1 ; i <= contador; i++){
+        if(contador % i == 0){
             cont++;
         }
     }
     if(cont == 2){
-        
-        for(i = 1; i<= valor ; i++){
-            if(valor % i == 0 ){
-               if(*p1 < i){
-                   *p1 = i;
-               }
-              else if(*p2 > i ){
-                  *p2 = i;
-              }
+        return true;
+    }
+    else
+        return false;
+}
+
+void maiorPrimoEmenorPrimo(int valor, int *p1, int *p2){
+    int i,j;
+    bool primo,primoj;
+
+    for(i = 1 ; i < valor; i++){
+
+        primo = ehPrimo(i);
+
+        if(primo == true){
+
+            if(i > *p1){
+                *p1 = i;
             }
         }
-        
+
+        for(j = valor + 1; j != 0; j++){
+
+            primoj = ehPrimo(j);
+            if( primoj == true){
+
+                if(j > valor){
+
+                    *p2 = j;
+                    break;
+
+
+                }
+            }
+        }
     }
-    
-    
+
 }
 int main()
 {
     int num1;
     int p1 = 0, p2 = 0;
-    
-    
+
+
     printf("Digite um um valor N:");
-        scanf("%d",&num1);
-    
-    maiorPrimoEmenorPrimo(num1,*p1,*p2);
-        
+    scanf("%d",&num1);
+
+    maiorPrimoEmenorPrimo(num1,&p1,&p2);
+
     printf("O valor de N:%d\n", num1);
     printf("o maior número primo menor que N:%d\no menor número primo maior que N:%d\n", p1, p2);
 
-    
-    }
+
+}
